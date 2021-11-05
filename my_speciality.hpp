3c95f8e159d9d177a90e980696aa17680c2d4adb
erop <Egor Sergeev>
2021-11-05 06:08:27 +0300

    File with player's speciality

---
+++ my_speciality.hpp
@@ -0,0 +1,10 @@
+#ifndef __MY_SPECIALITY_HPP__
+#define __MY_SPECIALITY_HPP__
+
+#include "model/Speciality.hpp"
+
+namespace constants {
+    constexpr model::Speciality my_speciality = model::Speciality::LOGISTICS;
+};
+
+#endif
